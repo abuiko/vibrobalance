e00e3a9b33ad8fac8be807c8a01dde245effe0da
Alena Buiko <abuiko.ma@gmail.com>
2021-01-13 16:04:30 -0500

    Change html markup / add sass (nav, hero, about)

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const nav = document.querySelector(".nav-links");
-const burger = document.querySelector(".burger");
-const links = nav.querySelectorAll("a");
+const burger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-open");
-  burger.classList.toggle("toggle");
-});
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    nav.classList.toggle("nav-open");
-    burger.classList.toggle("toggle");
-  });
+  header.classList.toggle("open");
+  body.classList.add("no-scroll");
+  if (header.classList.contains("open")) {
+    fadeElems.forEach((element, index) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  } else {
+    body.classList.remove("no-scroll");
+    fadeElems.forEach((element, index) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  }
 });
