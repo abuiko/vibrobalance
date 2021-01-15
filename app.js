453651994cdf5d4662abcb6f2f989e30d577fb5f
Alena Buiko <abuiko.ma@gmail.com>
2021-01-15 17:18:32 -0500

    Minor changes of design

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
+// const burger = document.querySelector("#btnHamburger");
+// const header = document.querySelector(".header");
+// const overlay = document.querySelector(".overlay");
+// const fadeElems = document.querySelectorAll(".has-fade");
+// const body = document.querySelector("body");
+
+// burger.addEventListener("click", () => {
+//   header.classList.toggle("open");
+//   body.classList.add("no-scroll");
+//   if (header.classList.contains("open")) {
+//     fadeElems.forEach((element, index) => {
+//       element.classList.remove("fade-out");
+//       element.classList.add("fade-in");
+//     });
+//   } else {
+//     body.classList.remove("no-scroll");
+//     fadeElems.forEach((element, index) => {
+//       element.classList.remove("fade-in");
+//       element.classList.add("fade-out");
+//     });
+//   }
+// });
 const burger = document.querySelector("#btnHamburger");
-const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
-const fadeElems = document.querySelectorAll(".has-fade");
-const body = document.querySelector("body");
+const nav = document.querySelector(".header");
+const popup = document.querySelector(".header__popup");
 
 burger.addEventListener("click", () => {
-  header.classList.toggle("open");
-  body.classList.add("no-scroll");
-  if (header.classList.contains("open")) {
-    fadeElems.forEach((element, index) => {
-      element.classList.remove("fade-out");
-      element.classList.add("fade-in");
-    });
-  } else {
-    body.classList.remove("no-scroll");
-    fadeElems.forEach((element, index) => {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
-  }
+  nav.classList.toggle("open");
+  popup.classList.toggle("active");
 });
