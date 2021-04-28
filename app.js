5a0f66fdde6f853c6ce376c96e5a65e3c536a8ff
Alena Buiko <abuiko.ma@gmail.com>
2021-04-28 17:35:29 -0400

    organize folders

--- app.js
+++ app.js
@@ -3,23 +3,6 @@
 // const overlay = document.querySelector(".overlay");
 // const fadeElems = document.querySelectorAll(".has-fade");
 // const body = document.querySelector("body");
-
-// burger.addEventListener("click", () => {
-//   header.classList.toggle("open");
-//   body.classList.add("no-scroll");
-//   if (header.classList.contains("open")) {
-//     fadeElems.forEach((element, index) => {
-//       element.classList.remove("fade-out");
-//       element.classList.add("fade-in");
-//     });
-//   } else {
-//     body.classList.remove("no-scroll");
-//     fadeElems.forEach((element, index) => {
-//       element.classList.remove("fade-in");
-//       element.classList.add("fade-out");
-//     });
-//   }
-// });
 const burger = document.querySelector("#btnHamburger");
 const nav = document.querySelector(".header");
 const popup = document.querySelector(".header__popup");
