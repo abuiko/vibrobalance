5ca99c9a007ec70c66943186ea342f15f3505b2d
Alena Buiko <abuiko.ma@gmail.com>
2021-05-06 18:23:04 -0400

    change popup nav window

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-// const burger = document.querySelector("#btnHamburger");
-// const header = document.querySelector(".header");
-// const overlay = document.querySelector(".overlay");
-// const fadeElems = document.querySelectorAll(".has-fade");
-// const body = document.querySelector("body");
 const burger = document.querySelector("#btnHamburger");
 const nav = document.querySelector(".header");
 const popup = document.querySelector(".header__popup");
+const span = document.querySelectorAll(".header__toggle span");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("open");
   popup.classList.toggle("active");
+
+  if (nav.classList.contains("open")) {
+    span.forEach(item => {
+      item.style.backgroundColor = "#000";
+    })
+  } else {
+    span.forEach(item => {
+      item.style.backgroundColor = "#fff";
+    })
+  }
+
 });
