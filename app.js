1a7d3fbb928380adc2036dd034c1e661139a9cb8
Alena Buiko <abuiko.ma@gmail.com>
2021-05-06 18:41:32 -0400

    add no-scroll when popup window open

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const burger = document.querySelector("#btnHamburger");
 const nav = document.querySelector(".header");
 const popup = document.querySelector(".header__popup");
 const span = document.querySelectorAll(".header__toggle span");
+const body = document.querySelector('body');
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("open");
@@ -11,10 +12,13 @@ burger.addEventListener("click", () => {
     span.forEach(item => {
       item.style.backgroundColor = "#000";
     })
+    body.classList.add("no-scroll");
   } else {
     span.forEach(item => {
       item.style.backgroundColor = "#fff";
+
     })
+    body.classList.remove("no-scroll");
   }
 
 });
